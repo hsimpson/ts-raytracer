b470b20b8ec4a5449b25c70122c3a5cf78747929
Daniel Toplak <homer.simpson123@gmx.de>
2020-07-31 21:49:35 +0200

    workaround: at the moment serializer do not work in optimized production

--- webpack.prod.ts
+++ webpack.prod.ts
@@ -2,7 +2,10 @@ import { commonConfig } from './webpack.common';
 import { merge } from 'webpack-merge';
 
 const prodConfig = merge(commonConfig, {
-  //mode: 'production',
+  // mode: 'production',
+  // FIXME: in production serializer no longer working!
+  mode: 'development',
+  //devtool: 'cheap-module-eval-source-map',
 });
 
 export default prodConfig;
