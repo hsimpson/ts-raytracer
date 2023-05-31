bc3331485b9a1f9017eae43fd4136516630fcfe0
Daniel Toplak <homer.simpson123@gmx.de>
2023-06-01 00:20:27 +0200

    fix: new version of simplex-noise

--- src/textures/noise.ts
+++ src/textures/noise.ts
@@ -1,16 +1,20 @@
 import { vec3 } from 'gl-matrix';
 // import { snoise } from './simplex3dnoise';
-import SimplexNoise from 'simplex-noise';
+import alea from 'alea';
+import { createNoise3D } from 'simplex-noise';
 import { serializable } from '../serializing';
 import { Perlin } from './perlin';
 import { Texture } from './texture';
 
-const RANDOMSEED = 'just a random seed string';
+// create a random number generator based on a seed string
+const prng = alea('just a random seed string');
+const noise3D = createNoise3D(prng);
+
 @serializable
 export class NoiseTexture extends Texture {
   private _noise = new Perlin();
   private _scale: number;
-  private _simplexNoise: SimplexNoise;
+  // private _simplexNoise: SimplexNoise;
 
   public constructor(scale: number) {
     super();
@@ -22,16 +26,16 @@ export class NoiseTexture extends Texture {
   }
 
   public turb(p: vec3, depth = 7): number {
-    if (!this._simplexNoise) {
-      this._simplexNoise = new SimplexNoise(RANDOMSEED);
-    }
+    // if (!this._simplexNoise) {
+    //   this._simplexNoise = new SimplexNoise(RANDOMSEED);
+    // }
 
     let accum = 0.0;
     const tempP = p;
     let weight = 1.0;
     for (let i = 0; i < depth; i++) {
       // accum += weight * snoise(tempP);
-      accum += weight * this._simplexNoise.noise3D(p[0], p[1], p[2]);
+      accum += weight * noise3D(p[0], p[1], p[2]);
       weight *= 0.5;
       vec3.scale(tempP, tempP, 2.0);
     }
@@ -43,7 +47,7 @@ export class NoiseTexture extends Texture {
     return vec3.scale(
       vec3.create(),
       vec3.scale(vec3.create(), vec3.fromValues(1, 1, 1), 0.5),
-      1.0 + Math.sin(this._scale * p[2] + 10.0 * this.turb(p))
+      1.0 + Math.sin(this._scale * p[2] + 10.0 * this.turb(p)),
     );
   }
 }
